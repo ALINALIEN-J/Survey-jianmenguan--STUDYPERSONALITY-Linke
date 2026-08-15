230723959e0b4c9424080cc099a713a28ea0671c
ALINALIEN-J <664520351@qq.com>
2026-08-16 00:18:20 +0800

    Correct app.run() syntax in main block
    Fix syntax error in app.run() call.

--- app.py
+++ app.py
@@ -332,4 +332,5 @@ init_db()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080))) port=int(os.environ.get("PORT", 8080)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    
